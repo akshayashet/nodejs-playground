1afcaf02295421e1e900c65c0ec5418cf02b8a47
Akshaya Shet <akshayashet@gmail.com>
2025-03-06 11:32:49 +0530

    refactor: update file paths and implement user API endpoints

--- devCon/src/app.js
+++ devCon/src/app.js
@@ -2,16 +2,49 @@ const express = require('express');
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-    console.log("This always runs!");
-    res.send("<h1>Hello from Express!</h1>");
+// this will match get http method api calls to /users
+app.get('/user', (req, res) => {
+    res.send({
+        firstName: 'Akshay',
+        lastName: 'Shet',
+    });
 });
 
-app.use("/add-product", (req, res, next) => {
-    console.log("In another middleware!");
-    res.send("<h1>The 'Add Product' Page</h1>");
+app.post('/user', (req, res) => {
+    // Saving user to database
+    res.send({
+        message: 'User created successfully!',
+    });
 });
 
-app.listen(3000, ()=>{
+app.delete('/user', (req, res) => {
+    // Deleting user from database
+    res.send({
+        message: 'User deleted successfully!',
+    });
+});
+
+app.put('/user', (req, res) => {
+    // Updating user in database
+    res.send({
+        message: 'User updated successfully!',
+    });
+});
+
+app.patch('/user', (req, res) => {
+    // Updating user in database
+    res.send({
+        message: 'User updated successfully!',
+    });
+});
+
+// this will match all http methods api calls to /hello
+app.use("/hello", (req, res) => {
+    console.log("Hello from the middleware!");
+    res.send("<h1>Hello from the middleware!</h1>");
+});
+
+
+app.listen(3000, () => {
     console.log('Server is up on port 3000');
 });
